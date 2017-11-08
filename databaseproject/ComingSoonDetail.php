e221137a8bf8d8a27d494bde5cf574cf2237071a
thanarath <33449768+thanarath@users.noreply.github.com>
2017-11-08 13:59:40 +0700

    Coming Soon detail
    page layout

---
+++ databaseproject/ComingSoonDetail.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>MovieHub</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="js/jquery-func.js"></script>
+<!--[if IE 6]><link rel="stylesheet" href="css/ie6.css" type="text/css" media="all" /><![endif]-->
+</head>
+<body>
+  <div id="shell">
+    <div id="header">
+      <div class="Logo">
+        <h1>MovieHub</h1>
+    </div>
+      <div class="social"> <span>FOLLOW US ON:</span>
+        <ul>
+          <li><a class="twitter" href="#">twitter</a></li>
+          <li><a class="facebook" href="#">facebook</a></li>
+          <li><a class="vimeo" href="#">vimeo</a></li>
+          <li><a class="rss" href="#">rss</a></li>
+        </ul>
+      </div>
+      <div id="navigation">
+        <ul>
+          <li><a href="Home">HOME</a></li>
+          <li><a href="movie.php">Now Showing</a></li>
+          <li><a class="active" href="#">Coming Soon</a></li>
+          <li><a href="#">Promotion</a></li>
+          <li><a href="#">Sign Up/Login</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      <div id="sub-navigation">
+        <ul>
+
+        </ul>
+        <div id="search">
+          <form action="#" method="get" accept-charset="utf-8">
+            <label for="search-field">SEARCH</label>
+            <input type="text" name="search field" value="Enter search here" id="search-field" class="blink search-field"  />
+            <input type="submit" value="GO!" class="search-button" />
+          </form>
+        </div>
+      </div>
+    </div>
+
+
+
+
+<div id="footer">
+    <p class="lf">Copyright &copy; 2017 <a href="#">SiteName</a> - All Rights Reserved</p>
+    <p class="rf">MovieHub <br>Krittikorn Kramgomut 5822771432<br>Thanapong Maleewan 5822790275 <br> Thanarath Piyakulpinyo</p>
+    <div style="clear:both;"></div>
+</div>
+<!-- END PAGE SOURCE -->
+</body>
+</html>
