476bda06c33720c1c4a8a68d80a92c9a6f1e8c26
thanarath <33449768+thanarath@users.noreply.github.com>
2017-11-11 12:43:11 +0700

    Coming soon detail
    add info text box

--- databaseproject/ComingSoonDetail.php
+++ databaseproject/ComingSoonDetail.php
@@ -56,8 +56,9 @@
           <div class="flexible-container">
             <iframe src="https://b922bde52f23a8481830-83cb7d8d544f653b52d1a1621f05ea9d.ssl.cf3.rackcdn.com/video/landingpage.mp4" frameborder="0" style="border:0"></iframe>
           </div>
-          <div class="box-moive">
-            Movie info
+          <div class="flexible-container">
+            Based upon the characters created by Neil Gaiman, Sam Kieth and Mike Dringenberg, LUCIFER is the story of the original fallen angel.
+              Bored and unhappy as the Lord of Hell, LUCIFER MORNINGSTAR has abandoned his throne and retired to L.A., where he owns Lux, an upscale nightclub. Charming, charismatic and devilishly handsome, Lucifer (Tom Ellis) is enjoying his retirement, indulging in a few of his favorite things wine, women and song when a beautiful pop star is brutally murdered outside of Lux. For the first time in roughly 10 billion years, he feels something awaken deep within him as a result of this murder. Compassion? Sympathy? The very thought disturbs him as well as his best friend and confidante, Mazikeen (Lesley-Ann Brandt), a fierce demon in the form of a beautiful young woman. The murder attracts the attention of LAPD homicide detective CHLOE DANCER, who initially is dismissive of Lucifer. But she becomes intrigued by his talent for drawing out peoples secrets and his desire to dispense justice, doling out punishment to those who deserve it. As they work together to solve the pop stars murder, Lucifer is struck by Chloes inherent goodness. Accustomed to dealing with the absolute worst of humanity, Lucifer is intrigued by Chloes apparent purity and begins to wonder if theres hope for his own soul yet. At the same time, Gods emissary, the angel Amendiel (D.B. Woodside), has been sent to Los Angeles to convince Lucifer to return to the underworld can the Devil incarnate be tempted toward the side of Good, or will his original calling pull him back toward Evil?
           </div>
           <div class="cl">&nbsp;</div>
         </div>
