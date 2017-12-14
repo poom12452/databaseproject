15dc6e6282d7735f217ac97d78a89947c638d801
thanarath <33449768+thanarath@users.noreply.github.com>
2017-12-14 17:43:53 +0700

    Add ticket
    ticket layout

---
+++ databaseproject/ticket.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>MovieHub</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/Seat.css" type="text/css" media="all" />
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="js/jquery-func.js"></script>
+<script src="js/jquery-1.11.0.min.js"></script>
+<script src="js/jquery.seat-charts.js"></script>
+<script src="js/jquery-1.11.0.min.js"></script>
+<script src="js/jquery.seat-charts.js"></script>
+<!--[if IE 6]><link rel="stylesheet" href="css/ie6.css" type="text/css" media="all" /><![endif]-->
+</head>
+<body>
+  <div id="shell">
+    <div id="header">
+      <div class="Logo">
+        <img src="image/Movie.png">
+    </div>
+      <div class="social"> <span>FOLLOW US ON:</span>
+        <ul>
+          <li><a class="twitter" href="#">twitter</a></li>
+          <li><a class="facebook" href="#">facebook</a></li>
+          <li><a class="vimeo" href="#">vimeo</a></li>
+          <li><a class="rss" href="#">rss</a></li>
+        </ul>
+      </div>
+      <div id="navigation">
+        <ul>
+          <li><a href="homepage.php">Home</a></li>
+          <li><a href="movie.php">Now Showing</a></li>
+          <li><a class="active" href="comingsoon.php">Coming Soon</a></li>
+          <li><a href="promotion.php">Promotion</a></li>
+          <li><a href="Login.php">Sign Up/Login</a></li>
+          <li><a href="Contact.php">Contact Us</a></li>
+        </ul>
+      </div>
+      <div id="sub-navigation">
+        <ul>
+
+        </ul>
+        <div id="search">
+          <form action="#" method="get" accept-charset="utf-8">
+            <label for="search-field">SEARCH</label>
+            <input type="text" name="search field" value="Enter search here" id="search-field" class="blink search-field"  />
+            <input type="submit" value="GO!" class="search-button" />
+          </form>
+        </div>
+      </div>
+    </div>
+    
+
+
+
+  <div id="footer">
+      <p class="lf">Copyright &copy; 2017 <a href="#">SiteName</a> - All Rights Reserved</p>
+      <p class="rf">MovieHub <br>Krittikorn Kramgomut 5822771432<br>Thanapong Maleewan 5822790275 <br> Thanarath Piyakulpinyo</p>
+      <div style="clear:both;"></div>
+  </div>
+  <!-- END PAGE SOURCE -->
+</body>
+  </html>
